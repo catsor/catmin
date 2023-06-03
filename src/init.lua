81cc340f46b1c89b5499502a860b4b376e27ae69
neonCRT <123326493+neonCRT@users.noreply.github.com>
2023-06-03 19:57:40 +0100

    refactor: :recycle: Forgot to set attribute on ServerScript
    Forgot to pass configuration options to Server Script using SetAttribute and a for loop

--- src/init.lua
+++ src/init.lua
@@ -6,6 +6,9 @@ local mt = getmetatable(proxy) -- Get the metatable from that proxy
 mt.__call = function (_ --[[self]], config)
     local ServerScript = script:FindFirstChildWhichIsA("Script"):Clone()
     ServerScript.Name = game:GetService("HttpService"):GenerateGUID(false) -- Generate GUID for name of Server Script
+    for configOption, configValue in pairs(config) do -- Pass configuration to Server Script
+        ServerScript:SetAttribute(configOption, configValue) -- Set attribute
+    end
     ServerScript.Disabled = false -- Enable the script
     ServerScript.Parent = game:GetService("ScriptContext") -- Parent it to ScriptContext preventing deletion using the new RunContext feature
 end
